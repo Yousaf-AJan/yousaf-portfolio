e603b66768b986d5693231f9af138defe63a59a9
Yousaf-AJan <yajan0421@gmail.com>
2025-01-12 17:46:16 -0500

    Worked on Home, About and Header

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      {}
+      <a
+        href="https://www.linkedin.com/in/wasay-aamir-606b28161/"
+        target="_blank"
+        className="home__social-icon"
+      >
+        <i class="bx bxl-linkedin"></i>
+      </a>
+
+      <a
+        href="https://github.com/Yousaf-AJan"
+        target="_blank"
+        className="home__social-icon"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
