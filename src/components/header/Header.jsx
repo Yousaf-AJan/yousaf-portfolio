df83a903d91ceda7ac7acf0d542c260995b85ae3
Yousaf-AJan <yajan0421@gmail.com>
2025-02-22 17:02:11 -0500

    Minor Changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
             <li className="nav__item">
               <Link
                 to="/"
-                onClick={() => setActiveNav("/")}
+                onClick={() => setActiveNav("/home")}
                 className={activeNav === "/" ? "nav__link active-link" : "nav__link"}
               >
                 <i className="uil uil-estate nav__icon"></i> Home
