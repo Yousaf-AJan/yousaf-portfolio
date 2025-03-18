d6fc2ba6f0b6ae081d2f232bd6cff86f7fee3930
Yousaf-AJan <yajan0421@gmail.com>
2025-03-18 15:18:59 -0400

    Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
